531455aab38b25a1046a97696b8c4b4b134c721d
unknown <moranidan1233@gmail.com>
2022-02-16 15:46:30 -0800

    home page

--- appApp/src/views/Home.vue
+++ appApp/src/views/Home.vue
@@ -1,6 +1,38 @@
 <template>
   <div class="home">
-      <h1>ברוכים הבאים לאפליקציה שלנו </h1>
+      <body-1> </br></br></body-1>
+      <body-1> </br></br></body-1>
+      <body-1> </br></br></body-1>
+      <body-1> </br></body-1>
+      <v-row>
+        <v-col
+          cols="4"
+        ></v-col>
+        <v-col
+          cols = "4"
+          <v-img
+            max-height="60"
+            max-width="50"
+            src="./click.png"
+          ></v-img>
+        ></v-col>
+        <v-col cols = "4"
+          <div class = 'b'>התנדבות בקליק</div>
+        ></v-col>
+      </v-row>
+      <body-1> </br></br></body-1>
+      <body-1> </br></br></body-1>
+      <body-1> </br></body-1>
+      <h2> .האג'נדה שלנו היא לחבר בצורה יעילה וחכמה בין העמותות ליחידות שמעוניינות להתנדב</br></h2>
+      <h2> .המטרה תושג ע"י שימוש באלגוריתם הייחודי שלנו שבעזרתו נוכל לשרת כמה שיותר עמותות בצורה מיטבית ובחלוקה הוגנת בין כולן</br></h2>
+      <h2> .בנוסף נייצר תמריץ עבור קצינות החינוך לחיזוק תודעת התרומה לקהילה ביחידה</br></h2>
+      <body-1> </br></br></body-1>
+      <body-1> </br></br></body-1>
+      <body-1> </br></br></body-1>
+      <body-1> </br></br></body-1>
+      <body-1> </br></br></body-1>
+      <body-1> </br></br></body-1>
+      <body-1></br></br>end</body-1>
   </div>
 </template>
 
@@ -9,3 +41,9 @@ export default {
   name: 'Home',
 }
 </script>
+
+<style>
+  div.b{
+    font-size: 80px;
+  }
+</style>
