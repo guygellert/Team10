2052e00351f4e0e7fa63076c97916fab2f87ab19
unknown <guygellert@gmail.com>
2022-02-16 14:12:50 +0200

    Add Python Project Server

---
+++ appApp/src/server/main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# TODO - return All Associations From DB
+# def getAssociations():
+
+
+# TODO - return specific association by name
+# def getAssociationsByName():
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
