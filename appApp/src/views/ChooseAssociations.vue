54fb97194c1360f21b2d24e8afad8d368968f454
unknown <moranidan1233@gmail.com>
2022-02-16 10:45:24 -0800

    new pages

---
+++ appApp/src/views/ChooseAssociations.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-row justify="center">
+    <v-col
+      cols="4"
+    ></v-col>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+    outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">
+          שם העמותה
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+          המידע על העמותה
+        </v-list-item-title>
+        <v-list-item-subtitle>התאריכים שהעמותות יכולות</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn
+        outlined
+        rounded
+        text
+      >
+        Button
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+  </v-row>
+</template>
+
+<script>
+  export default {}
+</script>
+
--- appApp/src/views/EductaionKatzin.vue
+++ appApp/src/views/EductaionKatzin.vue
@@ -1,69 +1,179 @@
 <template>
-<v-row>
+  <v-row justify="center">
     <v-col
-      cols="6"
-    >
-      <v-date-picker
-        v-model="dates"
-        multiple
-      ></v-date-picker>
-    </v-col>
+      cols="4"
+    ></v-col>
     <v-col
-      cols="5"
-
+      cols="4"
     >
-      <v-menu
-        ref="menu"
-        v-model="menu"
-        :close-on-content-click="false"
-        :return-value.sync="dates"
-        transition="scale-transition"
-        offset-y
-        min-width="auto"
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-combobox
+    <h1>רישום היחידות שרוצות להתנדב</h1>
+      <v-card ref="form">
+        <v-card-text>
+          <v-text-field
+            ref="unit"
+            v-model="unit"
+            :rules="[() => !!unit || 'This field is required']"
+            :error-messages="errorMessages"
+            label="שם היחידה"
+            required
+          ></v-text-field>
+          <v-text-field
+            ref="people_num"
+            v-model="people_num"
+            :rules="[() => !!people_num || 'This field is required', addressCheck]"
+            label="כמות האנשים"
+            required
+          ></v-text-field>
+          <v-text-field
+            ref="explanition"
+            v-model="explanition"
+            :error-messages="errorMessages"
+            label="דרישות היחידה במידה ויש"
+          ></v-text-field>
+          <v-date-picker
             v-model="dates"
             multiple
-            chips
-            small-chips
-            label="Multiple picker in menu"
-            prepend-icon="mdi-calendar"
-            readonly
-            v-bind="attrs"
-            v-on="on"
-          ></v-combobox>
-        </template>
-        <v-date-picker
-          v-model="dates"
-          multiple
-          no-title
-          scrollable
-        >
-          <v-spacer></v-spacer>
-          <v-btn
-            text
-            color="primary"
-            @click="menu = false"
+          ></v-date-picker>
+          <v-menu
+            ref="menu"
+            v-model="menu"
+            :close-on-content-click="false"
+            :return-value.sync="dates"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
           >
+            <template v-slot:activator="{ on, attrs }">
+              <v-combobox
+                v-model="dates"
+                multiple
+                chips
+                small-chips
+                label="Multiple picker in menu"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-combobox>
+            </template>
+            <v-date-picker
+              v-model="dates"
+              multiple
+              no-title
+              scrollable
+            >
+              <v-spacer></v-spacer>
+              <v-btn
+                text
+                color="primary"
+                @click="menu = false"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                text
+                color="primary"
+                @click="$refs.menu.save(dates)"
+              >
+                OK
+              </v-btn>
+            </v-date-picker>
+          </v-menu>
+        </v-card-text>
+        <v-divider class="mt-12"></v-divider>
+        <v-card-actions>
+          <v-btn text>
             Cancel
           </v-btn>
+          <v-spacer></v-spacer>
+          <v-slide-x-reverse-transition>
+            <v-tooltip
+              v-if="formHasErrors"
+              left
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  icon
+                  class="my-0"
+                  v-bind="attrs"
+                  @click="resetForm"
+                  v-on="on"
+                >
+                  <v-icon>mdi-refresh</v-icon>
+                </v-btn>
+              </template>
+              <span>Refresh form</span>
+            </v-tooltip>
+          </v-slide-x-reverse-transition>
           <v-btn
-            text
             color="primary"
-            @click="$refs.menu.save(dates)"
+            text
+            @click="submit"
           >
-            OK
+            Submit
           </v-btn>
-        </v-date-picker>
-      </v-menu>
+        </v-card-actions>
+      </v-card>
     </v-col>
+    <v-col
+      cols="12"
+    ></v-col>
   </v-row>
 </template>
 
-<script>
 
-export default {
-  name: 'Home',
-}
+<script>
+  export default {
+    data: () => ({
+      errorMessages: '',
+      name: null,
+      location: null,
+      people_num: null,
+      activity_kind: null,
+      phone_num: null,
+      explanition: null,
+      formHasErrors: false,
+      dates: [],
+      menu: false,
+    }),
+
+    computed: {
+      form () {
+        return {
+          name: this.name,
+          location: this.location,
+          people_num: this.people_num,
+          activity_kind: this.activity_kind,
+          phone_num: this.phone_num,
+          explanition: this.explanition,
+        }
+      },
+    },
+
+    watch: {
+      name () {
+        this.errorMessages = ''
+      },
+    },
+
+    methods: {
+      resetForm () {
+        this.errorMessages = []
+        this.formHasErrors = false
+
+        Object.keys(this.form).forEach(f => {
+          this.$refs[f].reset()
+        })
+      },
+      submit () {
+        this.formHasErrors = false
+        this.$router.push('/ChooseAssociations')
+        Object.keys(this.form).forEach(f => {
+          if (!this.form[f]) this.formHasErrors = true
+
+          this.$refs[f].validate(true)
+        })
+      },
+    },
+  }
 </script>
